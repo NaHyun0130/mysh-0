0e99167d36d6be0e93872aa6d1d229624ad16b87
NaHyun0130 <hyun0130@gmail.com>
2017-10-07 21:14:04 +0900

    Implement revise validate cd,pwd

--- src/commands.c
+++ src/commands.c
@@ -28,14 +28,14 @@ int do_pwd(int argc, char** argv) {
 
 int validate_cd_argv(int argc, char** argv) {
   
-  if(argc != 2) return 0;
+  if(argc == 2 || strcmp(argv[0],"cd") == 0) return 1;
  
-  return 1;
+  return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
  
-  if(argc != 1) return 0;
+  if(argc == 1 || strcmp(argv[0],"pwd") == 0) return 1;
 
-  return 1;
+  return 0;
 }
