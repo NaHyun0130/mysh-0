f8857905ea6a48ac60fbff27d087814e0fcf9713
NaHyun0130 <hyun0130@gmail.com>
2017-10-07 18:14:56 +0900

    Implements to fill utils

--- src/utils.c
+++ src/utils.c
@@ -3,5 +3,23 @@
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+  int count = 0;
+  char* temp;
+  char** a;
+
+  *argv = (char**)malloc(sizeof(char*));
+   a = *argv;
+   
+  temp = strtok(command, " \n");
+
+  while(temp != NULL)
+  {
+	test[count] = (char*)malloc(80*sizeof(char));
+	strcpy(test[count], temp);
+	count++;
+	temp = strtok(NULL," \n");	
+  }
+
+  *argc = count;
+
 }
