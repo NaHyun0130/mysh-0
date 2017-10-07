a753e6f32ef998b6a6539b37b880ae7ea39e6d6c
NaHyun0130 <hyun0130@gmail.com>
2017-10-07 14:26:22 +0900

    Implements to revise do_cd and add validate_cd,pwd

--- src/commands.c
+++ src/commands.c
@@ -4,16 +4,14 @@
 
 int do_cd(int argc, char** argv) {
 
-  int result = 0;
+  int result;
   if (!validate_cd_argv(argc, argv))
     return -1;
-  else{
- 	result = chdir(argv[1]);
-	if(result == -1) printf("Changing Fail\n");
-	else printf("Changing Completed\n");
 
-  }
-  
+  result = chdir(argv[1]);
+  if(result == -1) printf("Changing Fail\n");
+  else printf("Changing Completed\n");
+
   return 0;
 }
 
@@ -29,11 +27,14 @@ int do_pwd(int argc, char** argv) {
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  
+  if(argc != 2) return 0;
+ 
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+ 
+  if(argc != 1) return 0;
   return 1;
 }
